a091d028ee28038042aa8ee6d080bdc293333a72
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-09-23 21:29:53 +0100

    Add git config

--- home.nix
+++ home.nix
@@ -1,13 +1,37 @@
 { lib, pkgs, ... }:
+let
+  username = "filipe";
+in
 {
   home = {
     packages = with pkgs; [
-      hello
+      cowsay
+      git
+      home-manager
+      opam
     ];
 
-    username = "filipe";
-    homeDirectory = "/home/filipe";
+    username = "${username}";
+    homeDirectory = "/home/${username}";
 
     stateVersion = "25.05";
+
   };
+
+  programs.git = {
+    enable = true;
+    userName = "Filipe Marques";
+    userEmail = "filipe.s.marques@tecnico.ulisboa.pt";
+    ignores = [
+      # Dotfiles
+      ".files"
+      # Direnv stuff
+      ".direnv/"
+      ".envrc"
+    ];
+    extraConfig = {
+      credential.helper = "store";
+    };
+  };
+
 }
