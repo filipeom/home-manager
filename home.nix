563300dc9123bfede3957df37eeeb237325214bf
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-09-24 11:41:03 +0100

    Use modules

--- home.nix
+++ home.nix
@@ -1,79 +1,35 @@
 { lib, pkgs, config, ... }:
 {
-  home = {
+ home = {
     packages = with pkgs; [
       cowsay
       git
       home-manager
       opam
       oh-my-zsh
+      neovim
     ];
 
     username = "filipe";
     homeDirectory = "/home/${config.home.username}";
 
     stateVersion = "25.05";
-
   };
 
   # XDG
   xdg = import ./xdg.nix { inherit config pkgs lib; };
 
+  fonts.fontconfig = {
     enable = true;
-    configHome = "${config.home.homeDirectory}/.config";
-    cacheHome = "${config.home.homeDirectory}/.cache";
-    dataHome = "${config.home.homeDirectory}/.local/share";
-    stateHome = "${config.home.homeDirectory}/.local/state";
-    userDirs = {
-      enable = true;
-      desktop = "${config.home.homeDirectory}/resources";
-      documents = "${config.home.homeDirectory}/resources";
-      download = "${config.home.homeDirectory}/downloads";
-      music = "${config.home.homeDirectory}/areas/music";
-      pictures = "${config.home.homeDirectory}/resources/photos";
-      publicShare = "${config.home.homeDirectory}/";
-      templates = "${config.home.homeDirectory}/resources/templates";
-      videos = "${config.home.homeDirectory}/";
-    };
+    defaultFonts.monospace = ["Liberation Mono"];
+    defaultFonts.sansSerif = ["Liberation Sans"];
+    defaultFonts.serif = ["Liberation Serif"];
   };
 
-  # Git
-  programs.git = {
-    enable = true;
-    userName = "Filipe Marques";
-    userEmail = "filipe.s.marques@tecnico.ulisboa.pt";
-    ignores = [
-      # Dotfiles
-      ".files"
-      # Direnv stuff
-      ".direnv/"
-      ".envrc"
-    ];
-    extraConfig = {
-      credential.helper = "store";
-    };
-  };
-
-  programs.zsh = {
-    enable = true;
-    dotDir = ".config/zsh";
-    history.path = "${config.xdg.dataHome}/zsh/zsh_history";
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "direnv"
-      ];
-      theme = "robbyrussell";
-    };
-    shellAliases = {
-      e = "$EDITOR";
-      vi = "nvim";
-      vim = "nvim";
-    };
-    initContent = ''
-      bindkey -s ^f "tmux-sessionizer\n"
-    '';
+  # programs
+  programs = {
+    git = import ./programs/git.nix { inherit config pkgs lib; };
+    zsh = import ./programs/zsh.nix { inherit config pkgs lib; };
   };
 
   home.sessionPath = [
@@ -83,14 +39,10 @@
 
   home.sessionVariables = {
     EDITOR = "nvim";
-
     NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
     NODE_REPL_HISTORY = "${config.xdg.dataHome}/node_repl_history";
-
     OPAMROOT = "${config.xdg.dataHome}/opam";
-
     CARGO_HOME = "${config.xdg.dataHome}/cargo";
-
     XINITRC = "${config.xdg.configHome}/X11/xinitrc";
   };
 }
