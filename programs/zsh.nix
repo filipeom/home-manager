8e6d6c7fafe6e8168c9d0cde92740c8e9fe2b1c5
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-09-24 11:40:56 +0100

    Move zsh to another module

---
+++ programs/zsh.nix
@@ -0,0 +1,23 @@
+{ lib, pkgs, config, ... }:
+{
+  enable = true;
+  dotDir = ".config/zsh";
+  history.path = "${config.xdg.dataHome}/zsh/zsh_history";
+  oh-my-zsh = {
+    enable = true;
+    plugins = [
+      "git"
+        "direnv"
+    ];
+    theme = "robbyrussell";
+  };
+  shellAliases = {
+    e = "$EDITOR";
+    vi = "nvim";
+    vim = "nvim";
+  };
+  initContent = ''
+    bindkey -s ^f "tmux-sessionizer\n"
+    '';
+
+}
