d66508b386fa750012a38b7ee51fb70c79ce3052
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-10-02 11:29:06 +0100

    Remove lsp-zero and use native lsp

---
+++ dotfiles/nvim/lua/filipeom/lsp.lua
@@ -0,0 +1,30 @@
+vim.lsp.enable({ 'lua_ls', 'ocamllsp', 'rust_analyzer' })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function (event)
+    local opts = { buffer = event.buf, remap = false }
+
+    vim.keymap.set('n', 'T', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('n', '<F3>', function() vim.lsp.buf.format() end, opts)
+    vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
+
+    vim.api.nvim_set_hl(0, "LspInlayHint", {
+      bg = "NONE",
+      fg = "#999999",
+      italic = true,
+    })
+
+    vim.keymap.set('n', '<leader>i', function()
+      vim.lsp.inlay_hint.enable(
+        not vim.lsp.inlay_hint.is_enabled(opts),
+        opts
+      )
+    end, opts)
+  end
+})
--- dotfiles/nvim/lua/filipeom/plugins.lua
+++ dotfiles/nvim/lua/filipeom/plugins.lua
@@ -10,37 +10,38 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
-  use 'EdenEast/nightfox.nvim' -- Packer
+  use 'EdenEast/nightfox.nvim'
   use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
   use 'nvim-treesitter/playground'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-commentary'
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    requires = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' }, -- Required
-      {                          -- Optional
-        'williamboman/mason.nvim',
-        run = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+  use 'mason-org/mason.nvim'
+  -- use {
+  --   'VonHeikemen/lsp-zero.nvim',
+  --   branch = 'v2.x',
+  --   requires = {
+  --     -- LSP Support
+  --     { 'neovim/nvim-lspconfig' }, -- Required
+  --     {                          -- Optional
+  --       'williamboman/mason.nvim',
+  --       run = function()
+  --         pcall(vim.cmd, 'MasonUpdate')
+  --       end,
+  --     },
+  --     { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-      -- Autocompletion
-      -- Plug 'hrsh7th/cmp-nvim-lsp'
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'hrsh7th/cmp-cmdline' },
-      { 'hrsh7th/nvim-cmp' },   -- Required
-      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-      { 'hrsh7th/cmp-vsnip' },
-      { 'hrsh7th/vim-vsnip' },
+  --     -- Autocompletion
+  --     -- Plug 'hrsh7th/cmp-nvim-lsp'
+  use { 'hrsh7th/cmp-buffer' }
+  use { 'hrsh7th/cmp-path' }
+  use { 'hrsh7th/cmp-cmdline' }
+  use { 'hrsh7th/nvim-cmp' }   -- Required
+  use { 'hrsh7th/cmp-nvim-lsp' } -- Required
+  use { 'hrsh7th/cmp-vsnip' }
+  use { 'hrsh7th/vim-vsnip' }
 
-    }
-  }
+  --   }
+  -- }
   use 'romgrk/barbar.nvim'
   use 'nvim-tree/nvim-web-devicons'
   use 'wasp-platform/ecmasl-vim'
